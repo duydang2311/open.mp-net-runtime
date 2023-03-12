1a3feaca6633fc0751c7f3138ae5f5b85a7db887
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-03-12 22:26:42 +0700

    fix(entity): add missing vw parameter

--- src/c-api/entity.hpp
+++ src/c-api/entity.hpp
@@ -10,4 +10,4 @@ DLL_EXPORT void Entity_SetPosition(IEntity* entity, Vector3 position);
 DLL_EXPORT Vector3 Entity_GetRotation(const IEntity* entity);
 DLL_EXPORT void Entity_SetRotation(IEntity* entity, Vector3 rotation);
 DLL_EXPORT int Entity_GetVirtualWorld(const IEntity* entity);
-DLL_EXPORT void Entity_SetVirtualWorld(IEntity* entity);
+DLL_EXPORT void Entity_SetVirtualWorld(IEntity* entity, int vw);
